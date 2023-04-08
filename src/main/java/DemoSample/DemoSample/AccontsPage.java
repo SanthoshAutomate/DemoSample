748ec53cf6a04adf714c4782d88a0d11f32813db
unknown <santoshpr84@gmal.com>
2023-04-09 00:11:01 +0530

    Added Acc Page

---
+++ src/main/java/DemoSample/DemoSample/AccontsPage.java
@@ -0,0 +1,12 @@
+package DemoSample.DemoSample;
+
+public class AccontsPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Accounts Page");
+
+	}
+
+}
